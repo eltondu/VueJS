8c669a09a96125b573e649a158370dfd1ec160be
eltondu <elton.du@gmail.com>
2022-02-07 14:02:42 -0300

    07-02-2022

--- dom/desafio-03/app.js
+++ dom/desafio-03/app.js
@@ -1,12 +1,20 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: 0
+        valor: 0,
+        titulo: "Desafio 3"
     },
-    methods: {
-        mudaValor(){
+    computed: {
+        resultado() {
             //Retorna o valor
-            return this.valor
+            return this.valor == 37 ? 'Valor igual' : 'Valor diferente'
         }
     },
+    watch: {
+        resultado(){
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000);
+        }
+    }
 });
